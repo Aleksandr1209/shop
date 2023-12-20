ffbe89819dbc89b7f2eb5091fd53e566d458956e
Aleksandr1209 <114938226+Aleksandr1209@users.noreply.github.com>
2023-12-20 13:09:50 +0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const categoryRouter = require('./routes/category.routes')
 const productRouter = require('./routes/product.routes')
 const cors = require('cors');
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3002
 
 const app = express()
 app.use(cors());
